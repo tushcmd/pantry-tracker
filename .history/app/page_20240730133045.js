0a33fa9fcecb27e423e095208de6ae7d679c264c
tushcmd <muturidavid854@gmail.com>
2024-07-31 11:23:51 +0300

    Remove .history folder from tracking and update .gitignore

--- .history/app/page_20240730133045.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Box } from '@mui/material';
-
-export default function Home() {
-  return (
-    <Box></Box>
-  );
-}
--- .history/app/page_20240730164149.js
+++
@@ -1,9 +0,0 @@
-import { Box } from '@mui/material';
-
-export default function Home() {
-  return (
-    <Box>
-      Hello World
-    </Box>
-  );
-}
