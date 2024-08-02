9afa2e850275a6bb7755efe378f3268129c2775e
tushcmd <muturidavid854@gmail.com>
2024-08-02 21:48:35 +0300

    feat: added recipes and grocery-list pages

---
+++ app/recipes/page.js
@@ -0,0 +1,13 @@
+import { Box, Container, Typography } from '@mui/material'
+
+export default function Home() {
+    return (
+        <Container>
+            <Box sx={{ my: 4 }}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Recipes
+                </Typography>
+            </Box>
+        </Container>
+    )
+}
